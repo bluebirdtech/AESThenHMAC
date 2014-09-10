7b5e4d485e3d03aed23492bbe76761fb1aa2ebfb
bluebirdtech <mail@bluebirdtechnology.com>
2014-09-10 17:22:24 +0100

    Initial commit

---
+++ package.cmd
@@ -0,0 +1,8 @@
+if not exist Download mkdir Download
+if not exist Download\package mkdir Download\package
+if not exist Download\package\lib mkdir Download\package\lib
+if not exist Download\package\lib\net40-Client mkdir Download\package\lib\net40-Client
+
+copy AESThenHMAC\bin\Release\AESThenHMAC.dll Download\Package\lib\net40-Client\
+
+nuget.exe pack AESThenHMAC.nuspec -BasePath Download\Package -o Download
