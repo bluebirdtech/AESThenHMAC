3037ab8f6516c03e7e0fd509dca45b2d18fc89ed
bluebirdtech <mail@bluebirdtechnology.com>
2014-09-10 17:49:01 +0100

    Add xml documentation

--- package.cmd
+++ package.cmd
@@ -4,5 +4,6 @@ if not exist Download\package\lib mkdir Download\package\lib
 if not exist Download\package\lib\net40-Client mkdir Download\package\lib\net40-Client
 
 copy AESThenHMAC\bin\Release\AESThenHMAC.dll Download\Package\lib\net40-Client\
+copy AESThenHMAC\bin\Release\AESThenHMAC.xml Download\Package\lib\net40-Client\
 
 nuget.exe pack AESThenHMAC.nuspec -BasePath Download\Package -o Download
